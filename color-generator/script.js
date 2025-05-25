0ed7596214ce6419437ec46fc0ce9f20a4f2075a
hrveekshith <veekshithhrveekshithhr@gmail.com>
2025-05-25 18:29:43 +0530

    color-generator

--- color-generator/script.js
+++ color-generator/script.js
@@ -1,8 +1,12 @@
-function clr_generate(){
-    const gen_color = "#"+Math.floor(Math.random()*16777215).toString(16).padStart(6,"0");
+function generateColor() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+    document.body.style.backgroundColor = randomColor;
+    document.getElementById("colorText").innerText = randomColor;
+}
 
-    document.body.style.backgroundColor = gen_color;
 
-    document.getElementById("disp-text-1").innerText = gen_color;
-
+function copyToClipboard(){
+    const color = document.getElementById('colorText').innerText;
+    navigator.clipboard.writeText(color);
+    alert("Copied: "+color)
 }
